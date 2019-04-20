6a041b921975219d95ea93a50f19c6bf90cbdcdf
Josh Dresner <joshua.dresner@aol.com>
2019-04-20 15:34:24 -0400

    Added Acct Change

--- succes.py
+++ succes.py
@@ -16,6 +16,7 @@ with open('config.json') as w:
     ACCESS_SECRET = w["access_secret"]
     Btoken = w["Btoken"]
     webhookColor = w["webhookColor"]
+    accountName = w["AccountName"]
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
@@ -30,28 +31,21 @@ async def on_message(message):
         try:
             author = str(message.author)
             author = re.split(r'\b#\b', author, maxsplit=1)[0].strip()
-            
             image_url = message.attachments[0]['url']
             tweet_text = ('Success by '+author)
             tweet_image = image_url
-            
             filename = 'temp.jpg'
-            
             request = requests.get(tweet_image, stream=True)
             if request.status_code == 200:
                 with open(filename, 'wb') as image:
                     for chunk in request:
                         image.write(chunk)
-                        
                 upload_tweet = api.update_with_media(filename, status=tweet_text)
                 tweet_id = str(upload_tweet.id)
-                
-
                 embed=discord.Embed(title="Tweet Posted!", colour=discord.Colour(webhookColor))
-                embed.add_field(name = 'See your tweet here 😃 ', value = 'https://twitter.com/EclipseFnF/status/' + str(tweet_id))
-                embed.set_footer(text="Eclipse Success")
+                embed.add_field(name = 'See your tweet here 😃 ', value = 'https://twitter.com/'+str(accountName)+'/status/' + str(tweet_id))
+                embed.set_footer(text="{} Success".format(accountName))
                 print(str(message.author) + 'just posted success!' +  str(tweet_id))
-                
                 sent_msg =  client.send_message(message.channel, embed=embed)
                 await client.send_message(message.channel, embed=embed) 
         except IndexError:
@@ -65,5 +59,4 @@ async def on_ready():
 	print("Running on: ")
 	for botservers in client.servers: print(str(botservers))
 	print('------')
-     
 client.run(Btoken)
